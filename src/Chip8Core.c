187f9abb92433867af9f6cf47d5b090907261be1
David White <davidhw@live.unc.edu>
2020-04-16 16:33:40 +0000

    Add font data, implement Fx29, Fx30

--- src/Chip8Core.c
+++ src/Chip8Core.c
@@ -4,6 +4,12 @@
 #include "Chip8Core.h"
 #include "Guards.h"
 
+#define FONT_5_START  0x000
+#define FONT_10_START 0x080
+#define PROG_START    0x200
+
+static uint8_t font5[80], font10[100];
+
 Chip8Proc Chip8_init(uint8_t *program,
         size_t progSize,
         void (*sendScreen)(bool *screen),
@@ -19,7 +25,9 @@ Chip8Proc Chip8_init(uint8_t *program,
     // Init the proc's ram
     proc.ram = calloc(4096, sizeof(uint16_t));
     OOM_GUARD(proc.ram, __FILE__, __LINE__);
-    memcpy(proc.ram + 0x200, program, progSize);
+    memcpy(proc.ram + FONT_5_START, font5, sizeof(font5));
+    memcpy(proc.ram + FONT_10_START, font10, sizeof(font10));
+    memcpy(proc.ram + PROG_START, program, progSize);
     proc.PC = 0x200;
     // Init the screen buffer
     proc.screen = calloc(128 * 64, sizeof(bool));
@@ -36,7 +44,6 @@ void Chip8_end(Chip8Proc *self) {
     self->screen = NULL;
 }
 
-// Advance by one step
 void Chip8_advance(Chip8Proc *self) {
 
     // NOTE: see https://github.com/Chromatophore/HP48-Superchip for
@@ -250,11 +257,11 @@ void Chip8_advance(Chip8Proc *self) {
                     break;
                 case 0x29: // Fx29: Point I to 5-wide sprite for the hex char in Vx
                     validInst = true;
-                    // TODO Fx29
+                    self->I = FONT_5_START + 5 * op2;
                     break;
-                case 0x30: // Fx29: Point I to 10-wide sprite for the num char in Vx
+                case 0x30: // Fx30: Point I to 10-wide sprite for the num char in Vx
                     validInst = true;
-                    // TODO Fx30
+                    self->I = FONT_10_START + 10 * op2;
                     break;
                 case 0x33: // Fx30: Set I, I+1, I+2 to the decimal digits of Vx
                     validInst = true;
@@ -284,3 +291,45 @@ void Chip8_advance(Chip8Proc *self) {
     // Inc PC if neccessary
     if (normInc) { self->PC += 2; }
 }
+
+/*** Font data to be copied into the low ram addresses ***/
+static uint8_t font5[80] = {
+    /* 0 */ 0b11110000, 0b10010000, 0b10010000, 0b10010000, 0b11110000,
+    /* 1 */ 0b00100000, 0b01100000, 0b00100000, 0b00100000, 0b01110000,
+    /* 2 */ 0b11110000, 0b00010000, 0b11110000, 0b10000000, 0b11110000,
+    /* 3 */ 0b11110000, 0b00010000, 0b11110000, 0b00010000, 0b11110000,
+    /* 4 */ 0b10010000, 0b10010000, 0b11110000, 0b00010000, 0b00010000,
+    /* 5 */ 0b11110000, 0b10000000, 0b11110000, 0b00010000, 0b11110000,
+    /* 6 */ 0b11110000, 0b10000000, 0b11110000, 0b10010000, 0b11110000,
+    /* 7 */ 0b11110000, 0b00010000, 0b00100000, 0b01000000, 0b01000000,
+    /* 8 */ 0b11110000, 0b10010000, 0b11110000, 0b10010000, 0b11110000,
+    /* 9 */ 0b11110000, 0b10010000, 0b11110000, 0b00010000, 0b11110000,
+    /* A */ 0b11110000, 0b10010000, 0b11110000, 0b10010000, 0b10010000,
+    /* B */ 0b11100000, 0b10010000, 0b11100000, 0b10010000, 0b11100000,
+    /* C */ 0b11110000, 0b10000000, 0b10000000, 0b10000000, 0b11110000,
+    /* D */ 0b11100000, 0b10010000, 0b10010000, 0b10010000, 0b11100000,
+    /* E */ 0b11110000, 0b10000000, 0b11110000, 0b10000000, 0b11110000,
+    /* F */ 0b11110000, 0b10000000, 0b11110000, 0b10000000, 0b10000000
+};
+static uint8_t font10[100] = {
+    /* 0 */ 0b00111100, 0b01111110, 0b11100111, 0b11000011, 0b11000011,
+    0b11000011, 0b11000011, 0b11100111, 0b01111110, 0b00111100,
+    /* 1 */ 0b00011000, 0b00111000, 0b01011000, 0b00011000, 0b00011000,
+    0b00011000, 0b00011000, 0b00011000, 0b00011000, 0b00111100,
+    /* 2 */ 0b00111110, 0b01111111, 0b11000011, 0b00000110, 0b00001100,
+    0b00011000, 0b00110000, 0b01100000, 0b11111111, 0b11111111,
+    /* 3 */ 0b00111100, 0b01111110, 0b11000011, 0b00000011, 0b00001110,
+    0b00001110, 0b00000011, 0b11000011, 0b01111110, 0b00111100,
+    /* 4 */ 0b00000110, 0b00001110, 0b00011110, 0b00110110, 0b01100110,
+    0b11000110, 0b11111111, 0b11111111, 0b00000110, 0b00000110,
+    /* 5 */ 0b11111111, 0b11111111, 0b11000000, 0b11000000, 0b11111100,
+    0b11111110, 0b00000011, 0b11000011, 0b01111110, 0b00111100,
+    /* 6 */ 0b00111110, 0b01111100, 0b11000000, 0b11000000, 0b11111100,
+    0b11111110, 0b11000011, 0b11000011, 0b01111110, 0b00111100,
+    /* 7 */ 0b11111111, 0b11111111, 0b00000011, 0b00000110, 0b00001100,
+    0b00011000, 0b00110000, 0b01100000, 0b01100000, 0b01100000,
+    /* 8 */ 0b00111100, 0b01111110, 0b11000011, 0b11000011, 0b01111110,
+    0b01111110, 0b11000011, 0b11000011, 0b01111110, 0b00111100,
+    /* 9 */ 0b00111100, 0b01111110, 0b11000011, 0b11000011, 0b01111111,
+    0b00111111, 0b00000011, 0b00000011, 0b00111110, 0b01111100
+};
